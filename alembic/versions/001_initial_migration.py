bb9953d54de4e0fbc25ebc173912f2ea275ad2c6
alanr <alanrios2001@gmail.com>
2024-08-27 21:49:03 -0300

    alembic migrations

---
+++ alembic/versions/001_initial_migration.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 4c2dc81924d0
+Revises: 
+Create Date: 2024-08-27 21:47:27.820231
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4c2dc81924d0'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
